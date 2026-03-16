d53a79c6540d5cbafef068df161b1c7b3cc9ff2a
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-15 19:43:56 -0500

    Add Rectangulo class with constructor and methods
    Implement Rectangulo class with corner coordinates and basic methods.

---
+++ LAB1/ACTIVIDADES/Rectangulo.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Rectangulo{
+    private Coordenada esquina1;
+    private Coordenada esquina2;
+    
+    // Constructor
+    public Rectangulo(Coordenada c1, Coordenada c2) {
+        setEsquina1(c1);
+        setEsquina2(c2);
+    }
+    
+    public void setEsquina1 (Coordenada coo) {
+        this.esquina1 = esquina1;
+    }
+    
+    public void setEsquina2 (Coordenada coo) {
+        this.esquina2 = esquina2;
+    }
+
+    public Coordenada getEsquina1() {
+        return esquina1;
+    }
+
+    public Coordenada getEsquina2() {
+        return esquina2;
+    }
+
+    public String toString() {
+    // fill in the code here
+    }
+}
