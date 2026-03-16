829bf89732280291df0d4faaf8e3f59ee13ead3b
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-15 23:07:50 -0500

    Add Verificador class with rectangle methods

---
+++ LAB1/ACTIVIDADES/Verificador.java
@@ -0,0 +1,17 @@
+public class Verificador {
+    
+    // esSobrePos: Los rectángulos se sobreponen
+    public static boolean esSobrePos(Rectangulo r1, Rectangulo r2){
+        return false; //avanzar
+    }
+
+    // esDisjunto: Los rectángulos están separados
+    public static boolean esDisjunto(Rectangulo r1, Rectangulo r2){
+        return false; //avanzar
+    }
+
+    public static boolean esJunto(Rectangulo r1, Rectangulo r2){
+        return true; //avanzar
+    }
+
+}
