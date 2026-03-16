90219fdf5491e6f59e3641ac9244f5e1d70e80b3
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-16 17:52:07 -0500

    Implement rectangle disjoint and touch checks
    Implemented the esDisjunto method to check if two rectangles are disjoint and updated the esJunto method to determine if they touch.

--- LAB1/ACTIVIDADES/Verificador.java
+++ LAB1/ACTIVIDADES/Verificador.java
@@ -37,11 +37,46 @@ public class Verificador {
 
     // esDisjunto: Los rectángulos están separados
     public static boolean esDisjunto(Rectangulo r1, Rectangulo r2){
-        return false; //avanzar
+
+        //Coordenadas de esuqina1
+        double a1x = r1.getEsquina1().getX();
+        double a1y = r1.getEsquina1().getY();
+        double a2x = r1.getEsquina2().getX();
+        double a2y = r1.getEsquina2().getY();
+
+        //Coordenadas de esquina2
+        double b1x = r2.getEsquina1().getX();
+        double b1y = r2.getEsquina1().getY();
+        double b2x = r2.getEsquina2().getX();
+        double b2y = r2.getEsquina2().getY();
+
+        //Ordenar puntos en el plano - Rectangulo A
+        double aIzq = Math.min(a1x, a2x);
+        double aDer = Math.max(a1x, a2x);
+        double aBajo = Math.min(a1y, a2y);
+        double aRriba = Math.max(a1y, a2y);
+
+        //Ordenar puntos en el plano - Rectangulo B
+        double bIzq = Math.min(b1x, b2x);
+        double bDer = Math.max(b1x, b2x);
+        double bBajo = Math.min(b1y, b2y);
+        double bRriba = Math.max(b1y, b2y);
+
+        //Si un rectángulo está completamente separado del otro son disjuntos
+        if((aDer < bIzq) || (bDer < aIzq) || (aRriba < bBajo) || (bRriba < aBajo)){
+            return true;
+        }
+
+        return false;
     }
 
+    // esJunto: Los rectángulos se tocan
     public static boolean esJunto(Rectangulo r1, Rectangulo r2){
-        return true; //avanzar
-    }
 
+        //No se sobreponen ni estan separados por tanto deben estar juntos
+        if(!esDisjunto(r1,r2) && !esSobrePos(r1,r2)){
+            return true;
+        }
+        return false;
+    }
 }
