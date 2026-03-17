615582030f37217e6b57501cefc6cafa6de57878
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-17 07:41:05 -0500

    Update file path for datos.txt in Main.java

--- LAB1/EJERCICIOS/Ejercicio2/Main.java
+++ LAB1/EJERCICIOS/Ejercicio2/Main.java
@@ -3,7 +3,7 @@ package LAB1.EJERCICIOS.Ejercicio2;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Nombre del archivo de texto y tamaño de la región a buscar
-        String nombreArchivo = "LAB1/EJERCICIOS/Ejercicio2/datos.txt"; // Ruta al archivo de texto con los datos (modificar según sea necesario)
+        String nombreArchivo = "datos.txt"; // Ruta al archivo de texto con los datos (modificar según sea necesario)
         int k = 2; // Tamaño de la región k x k
 
         // Creamos la instancia y ejecutamos
