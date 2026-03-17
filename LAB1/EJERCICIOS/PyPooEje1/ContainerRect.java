81885b04ff5833d19207531132d94ff2b65aae2e
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-16 22:56:10 -0500

    Add constructor to initialize ContainerRect fields

--- LAB1/EJERCICIOS/PyPooEje1/ContainerRect.java
+++ LAB1/EJERCICIOS/PyPooEje1/ContainerRect.java
@@ -4,7 +4,14 @@ public class ContainerRect {
     double[] distancias;
     double[] areas;
     int n;
-
+    
     static int numRec = 0;
-
+    
+    public ContainerRect(int n){
+        this.n = n;
+        rectangulos = new Rectangulo[n];
+        distancias = new double[n];
+        areas = new double[n];
+    }
+    
 }
