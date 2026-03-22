4ca7cf09c58529f94303b14b2c7d99674e41889e
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-22 18:33:21 -0500

    Create Principal.java

---
+++ LAB2/ACT/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+    public static void main(String[] args) {
+        Bolsa <Chocolatina> bolsaCho = new Bolsa <Chocolatina> (4); // insertar el limite de objetos
+        Chocolatina c = new Chocolatina("milka");
+        Chocolatina c1 = new Chocolatina("milka");
+        Chocolatina c2 = new Chocolatina("ferrero");
+        bolsaCho.add(c);
+        bolsaCho.add(c1);
+        bolsaCho.add(c2);
+
+        for (Chocolatina chocolatina: bolsaCho) {
+            System.out.println(chocolatina.getMarca());
+        }
+    }
+}
