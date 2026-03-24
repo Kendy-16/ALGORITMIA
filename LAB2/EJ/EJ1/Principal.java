9fe04ec0b7fff65184bc83cb64e0f87cc4b79c7f
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-23 19:25:33 -0500

    Update Principal.java

--- LAB2/EJ/EJ1/Principal.java
+++ LAB2/EJ/EJ1/Principal.java
@@ -1,36 +1,81 @@
 public class Principal {
+    
+    // Método genérico solicitado
+    public static <T> boolean exist(T[] arreglo, T elemento) {
+        
+        for (int i = 0; i < arreglo.length; i++) {
+        
+            if (arreglo[i].equals(elemento)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    // Método genérico recorredor
+    public static <T> void mostrarBolsa(Bolsa<T[]> bolsa) {
+        
+        for (T[] arreglo : bolsa) {
+            
+            for (int i = 0; i < arreglo.length; i++) {
+                System.out.println(arreglo[i]);
+            }
+        }
+    }
     public static void main(String[] args) {
-
-        // Bolsa de Chocolatinas
-        Bolsa<Chocolatina> bolsaCho = new Bolsa<Chocolatina>(8);
-        Chocolatina c = new Chocolatina("milka");
-        Chocolatina c1 = new Chocolatina("milka");
-        Chocolatina c2 = new Chocolatina("ferrero");
-
-        bolsaCho.add(c);
-        bolsaCho.add(c1);
-        bolsaCho.add(c2);
-
-        System.out.println("_________________________________");
-        for (Chocolatina chocolatina : bolsaCho) {
-            System.out.println(chocolatina.getMarca());
-        }
-
-        System.out.println("_________________________________");
-
-        // Bolsa de Golosinas
-        Bolsa<Golosina> bolsaGolo = new Bolsa<Golosina>(8);
-        Golosina g1 = new Golosina("gomitas", 0.5);
-        Golosina g2 = new Golosina("caramelos", 0.2);
-        Golosina g3 = new Golosina("chicle", 0.1);
-
-        bolsaGolo.add(g1);
-        bolsaGolo.add(g2);
-        bolsaGolo.add(g3);
-
-        for (Golosina g : bolsaGolo) {
-            System.out.println(g.getNombre() + " - " + g.getPeso() + " g");
-        }
-        System.out.println("_________________________________");
+        
+        // Bolsa de arreglos de Chocolatinas
+        Bolsa<Chocolatina[]> bolsaCho = new Bolsa<Chocolatina[]>(4);
+        
+        // Crear arreglo de chocolatinas
+        Chocolatina[] arrCho = {
+            new Chocolatina("milka"),
+            new Chocolatina("ferrero"),
+            new Chocolatina("iberico")
+        };
+        
+        // Agregar arreglo a la bolsa
+        bolsaCho.add(arrCho);
+        System.out.println("_________________________________\n");
+        
+        // Recorrer bolsa
+        mostrarBolsa(bolsaCho);
+        System.out.println("_________________________________\n");
+        
+        // Bolsa de arreglos de Golosinas
+        Bolsa<Golosina[]> bolsaGolo = new Bolsa<Golosina[]>(4);
+        
+        // Crear arreglo de golosinas
+        Golosina[] arrGolo = {
+            new Golosina("gomitas", 0.5),
+            new Golosina("caramelos", 0.2),
+            new Golosina("chicle", 0.1)
+        };
+        
+        // Agregar arreglo
+        bolsaGolo.add(arrGolo);
+        
+        // Mostrar contenido
+        mostrarBolsa(bolsaGolo);
+        System.out.println("_________________________________\n");
+        
+        System.out.println("PRUEBA DEL METODO EXIST");
+        System.out.println("_________________________________\n");
+        
+        // Buscar en arreglo de Chocolatinas
+        boolean existeCho = exist(arrCho, new Chocolatina("milka"));
+        boolean existeFo = exist(arrCho, new Chocolatina("fondu"));
+        System.out.println("¿Existe milka en Chocolatina?: " + existeCho);
+        System.out.println("¿Existe fondu en Chocolatina?: " + existeFo);
+        
+        
+        // Buscar en arreglo de Golosinas
+        boolean existeGolo = exist(arrGolo,new Golosina("gomitas", 0.5));
+        boolean existeChi = exist(arrGolo,new Golosina("chicle", 0.8));
+        System.out.println("\n¿Existe gomitas en Golosina?: " + existeGolo);
+        System.out.println("¿Existe chicle en Golosina?: " + existeChi);
+        
+        System.out.println("_________________________________\n");
     }
 }
