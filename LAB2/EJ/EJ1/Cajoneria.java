a379cc7e716a40ab944cd42ef861bf5fc68f7685
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-23 20:32:37 -0500

    Update Cajoneria.java

--- LAB2/EJ/EJ1/Cajoneria.java
+++ LAB2/EJ/EJ1/Cajoneria.java
@@ -1,24 +1,59 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Cajoneria<T> implements Iterable<T> {
+public class Cajoneria<T> implements Iterable<Caja<T>> {
 
-    private ArrayList<T> lista = new ArrayList<T>();
+    private ArrayList<Caja<T>> lista = new ArrayList<Caja<T>>();
     private int tope;
 
     public Cajoneria(int tope) {
         this.tope = tope;
     }
 
-    public void add(T objeto) {
+    public void add(Caja<T> caja) {
         if (lista.size() < tope) {
-            lista.add(objeto);
+            lista.add(caja);
         } else {
             throw new RuntimeException("no caben más cajas");
         }
     }
 
-    public Iterator<T> iterator() {
+    // Buscar
+    public String search(Object obj) {
+        for (int i = 0; i < lista.size(); i++) {
+            Caja<?> caja = lista.get(i);
+            if (caja.obtener().equals(obj)) {
+                
+                return "Posicion: " + i + " Color: " + caja.getColor();
+            }
+        }
+        return "Elemento no encontrado";
+    }
+
+    // Borrar
+    public Object delete(Object obj) {
+        for (int i = 0; i < lista.size(); i++) {
+            Caja<?> caja = lista.get(i);
+            if (caja.obtener().equals(obj)) {
+                Object eliminado = caja.obtener();
+                caja.guardar(null);
+                return eliminado;
+            }
+        }
+        return null;
+    }
+
+    // ToString
+    public String toString() {
+        String resultado = "";
+        for (int i = 0; i < lista.size(); i++) {
+            Caja<?> caja = lista.get(i);
+            resultado += "Posicion: " + i + " | Color: " + caja.getColor() + " | Contenido: " + caja.obtener()+ " |\n";
+        }
+        return resultado;
+    }
+
+    public Iterator<Caja<T>> iterator() {
         return lista.iterator();
     }
 }
