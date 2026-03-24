f0b908e7613b5be0c70b2c2f46a5642a55ebf218
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-03-23 19:26:46 -0500

    Update Chocolatina.java

--- LAB2/EJ/EJ1/Chocolatina.java
+++ LAB2/EJ/EJ1/Chocolatina.java
@@ -12,4 +12,19 @@ public class Chocolatina {
     public void setMarca(String marca) {
         this.marca = marca;
     }
+
+    public String toString() {
+        return "marca: " + marca;
+    }
+
+    // Metodo equals
+    public boolean equals(Object o) {
+
+        if (o instanceof Chocolatina) {
+            Chocolatina c = (Chocolatina) o;
+            return this.marca.equals(c.marca);
+        }
+        
+        return false;
+    }
 }
