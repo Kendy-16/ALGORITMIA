63c1c96eef407b1785fcbe731a86e50615b82405
Kendy-16 <kelly.ramos.1652@gmail.com>
2026-06-14 22:58:02 -0500

    Update Main.java

--- LAB10/ACT/Main.java
+++ LAB10/ACT/Main.java
@@ -1,1 +1,23 @@
-a
+import graph.GraphLink;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GraphLink<String> g = new GraphLink<>();
+
+        // INSERTAR VERTICES
+        g.insertVertex("A");
+        g.insertVertex("B");
+        g.insertVertex("C");
+        g.insertVertex("D");
+
+        // INSERTAR ARISTAS
+        g.insertEdge("A", "B");
+        g.insertEdge("A", "C");
+        g.insertEdge("B", "D");
+
+        // MOSTRAR
+        System.out.println(g);
+    }
+}
